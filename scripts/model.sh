d687ca42066e7952e803a95e98a455dd06fe81c4
Nishant Balepur <55101514+nbalepur@users.noreply.github.com>
2024-02-22 22:05:03 -0500

    Update model.sh

--- scripts/model.sh
+++ scripts/model.sh
@@ -18,14 +18,14 @@ hf_dataset_name="nbalepur/MCQA_quality"
 # can be "full" or in halves (e.g. "first_half"), quarters (e.g. "first_quarter"), or eigths (e.g. "first_eighth")
 partition="full" 
 
-hf_token="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" # huggingface token (for downloading gated models)
+hf_token="" # huggingface token (for downloading gated models)
 load_in_8bit="False" # load the model in 8bit? ("False" or "True")
 load_in_4bit="False" # load the model in 4bit? ("False" or "True")
 use_20_fewshot="False" # use a 20-shot prompt in ARC? ("False" or "True") => we set this to "True" for Falcon 
 
-res_dir="/fs/clip-projects/rlab/nbalepur/Artifact_Dataset_Creation/mcqa-artifacts/results/" # Results folder directory
-prompt_dir="/fs/clip-projects/rlab/nbalepur/Artifact_Dataset_Creation/mcqa-artifacts/prompts/" # Prompt folder directory
-cache_dir="/fs/clip-scratch/nbalepur/cache/" # Cache directory to save the model
+res_dir="/mcqa-artifacts/results/" # Results folder directory
+prompt_dir="/mcqa-artifacts/prompts/" # Prompt folder directory
+cache_dir="" # Cache directory to save the model
 
 datasets_str=$(IFS=" "; echo "${datasets[*]}")
 experiments_str=$(IFS=" "; echo "${experiments[*]}")
